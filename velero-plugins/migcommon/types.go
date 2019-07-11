31ce40b5ad64526b709e490d45e6c06dd2f27391
Scott Seago <sseago@redhat.com>
2019-07-11 17:00:22 -0400

    Use chosen storage class name from controller for PVC
    Override PVC storageclass (and accessmodes, if provided)
    with controller-provided annotation values.

--- velero-plugins/migcommon/types.go
+++ velero-plugins/migcommon/types.go
@@ -7,6 +7,11 @@ const SwingPVAnnotation string = "openshift.io/swing-pv"
 // copy, swing, TODO: others (snapshot, custom, etc.)
 const MigrateTypeAnnotation string = "openshift.io/migrate-type"
 
+// target storage class
+const MigrateStorageClassAnnotation string = "openshift.io/target-storage-class"
+//target access mode
+const MigrateAccessModeAnnotation string = "openshift.io/target-access-mode"
+
 //stage, final. Only valid for copy type.
 const MigrateCopyPhaseAnnotation string = "openshift.io/migrate-copy-phase"
 
