1ac635f5e53609e7911cdc16b9463e13cb7a4ed7
Dylan Murray <dymurray@redhat.com>
2020-03-24 11:14:28 -0400

    Include namespace check

--- velero-plugins/migsa/backup.go
+++ velero-plugins/migsa/backup.go
@@ -97,6 +97,9 @@ func (p *BackupPlugin) UpdateSCCMap() error {
 			// if second element is serviceaccount then last element is serviceaccountname
 			if splitUsername[1] == "serviceaccount" {
 				namespace := splitUsername[2]
+				if namespace == "" {
+					continue
+				}
 				if p.SCCMap[namespace] == nil {
 					p.SCCMap[namespace] = make(map[string][]apisecurity.SecurityContextConstraints)
 				}
